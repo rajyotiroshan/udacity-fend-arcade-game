ab612b017a86e4b981e9d5f65d525ecc3260a5fc
rajan <rajangreedge@gmail.com>
2018-12-05 11:32:39 +0530

    added x and y var to Enemy C.F

--- js/app.js
+++ js/app.js
@@ -6,6 +6,10 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    //x-postion of enemy
+    this.x = 0;
+    //y-position of enemy.
+    this.y = 0;
 };
 
 // Update the enemy's position, required method for game
