a38365ea0a6330abc4f765839c07c585954127a1
rajan <rajangreedge@gmail.com>
2018-12-05 11:34:08 +0530

    enemy update func

--- js/app.js
+++ js/app.js
@@ -18,6 +18,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x * dt;
 };
 
 // Draw the enemy on the screen, required method for game
